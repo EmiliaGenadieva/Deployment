14cfd07719894e1f0a33573111c9c83b66e699f1
EmiliaGenadieva <119783830+EmiliaGenadieva@users.noreply.github.com>
2023-05-31 11:12:29 +0200

    Update app.py

--- app.py
+++ app.py
@@ -50,33 +50,33 @@ def charts():
     if request.method == 'GET':
         return render_template('charts.html')
 
-@app.route('/',methods = ['POST', 'GET'])
-@app.route('/chart3')
+@app.route('/chart3', methods = ['POST', 'GET'])
 def chart3():
-    url = 'https://www.internetlivestats.com/total-number-of-websites/'
-    s = requests.Session()
-    response = s.get(url, timeout=10)
-    soup = BeautifulSoup(response.content, 'html.parser')
-    right_table=soup.find('table', {"class":'table table-striped table-bordered table-hover table-condensed table-list'})
-    rows = right_table.findAll("tr")
-    lst_data1 = []
-    for row in rows:  
-        data = [d.text.rstrip() for d in row.select('td')]
-        lst_data1.append(data)
-    lst_data1 = pd.DataFrame(lst_data1)
-    df = lst_data1.copy()
-    headers=df.iloc[0]
-    df  = pd.DataFrame(df.values[1:], columns=headers)
-    df['Websites'] = df["Websites"].str.replace(',','')
-    df['Websites'] = pd.to_numeric(df['Websites'])
-    fig = px.bar(df[:-1], x="Year (June)", y="Websites", barmode = 'group')
-    graphJSON = json.dumps(fig, cls=plotly.utils.PlotlyJSONEncoder)
-    header="Vegetables in Europe"
-    description = """
-    The rumor that vegetarians are having a hard time in London and Madrid can probably not be
-    explained by this chart.
-    """
-    return render_template('notdash2.html', graphJSON=graphJSON, header=header,description=description)
+    if request.method == 'GET':
+        url = 'https://www.internetlivestats.com/total-number-of-websites/'
+        s = requests.Session()
+        response = s.get(url, timeout=10)
+        soup = BeautifulSoup(response.content, 'html.parser')
+        right_table=soup.find('table', {"class":'table table-striped table-bordered table-hover table-condensed table-list'})
+        rows = right_table.findAll("tr")
+        lst_data1 = []
+        for row in rows:  
+            data = [d.text.rstrip() for d in row.select('td')]
+            lst_data1.append(data)
+        lst_data1 = pd.DataFrame(lst_data1)
+        df = lst_data1.copy()
+        headers=df.iloc[0]
+        df  = pd.DataFrame(df.values[1:], columns=headers)
+        df['Websites'] = df["Websites"].str.replace(',','')
+        df['Websites'] = pd.to_numeric(df['Websites'])
+        fig = px.bar(df[:-1], x="Year (June)", y="Websites", barmode = 'group')
+        graphJSON = json.dumps(fig, cls=plotly.utils.PlotlyJSONEncoder)
+        header="Vegetables in Europe"
+        description = """
+        The rumor that vegetarians are having a hard time in London and Madrid can probably not be
+        explained by this chart.
+        """
+        return render_template('notdash2.html', graphJSON=graphJSON, header=header,description=description)
 
 
 if __name__ == '__main__':
