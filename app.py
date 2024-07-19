29de4ac633caa670e1cf87f3ba2f0540f53b1273
EmiliaGenadieva <emilia.genadieva@gmail.com>
2024-07-19 15:36:30 +0200

    new code

--- app.py
+++ app.py
@@ -12,15 +12,16 @@ from flask import Flask, redirect, render_template, url_for, request
 
 app = Flask(__name__)
 # define ML Model saved on Firebase storage
-file = 'https://firebasestorage.googleapis.com/v0/b/inappvegetarian.appspot.com/o/filename.skops?alt=media&token=787bbd34-a73f-4175-a004-5196f8698231'
-clf = load(urllib.request.urlopen(file))
+#file = 'https://firebasestorage.googleapis.com/v0/b/inappvegetarian.appspot.com/o/filename.skops?alt=media&token=787bbd34-a73f-4175-a004-5196f8698231'
+#clf = load(urllib.request.urlopen(file))
+home_page = 'home.html'
 
 # declare the main route of the app
 @app.route('/', methods = ['POST', 'GET'])
 def home0():
     if request.method == 'GET':
-        return render_template('home.html')     
-    return render_template('home.html')
+        return render_template(home_page)     
+    return render_template(home_page)
 
 # route to print the API response of the predicted prices and their values
 @app.route('/dict')
@@ -34,8 +35,8 @@ def home(price):
         if price == None:
             return render_template('index.html', price = price)
         else:
-            return render_template('home.html', price = price)
-    return render_template('home.html', price = price)
+            return render_template(home_page, price = price)
+    return render_template(home_page, price = price)
 
 # declare route to get values of the form to calculate the predicted price
 @app.route('/index', methods = ['POST', 'GET'])
@@ -44,9 +45,10 @@ def index():
         salary = request.form['salary']
         tax =  request.form['tax']
         bonus =  request.form['bonus']
-        record={"Living_area": salary, "Bedrooms":tax, "Surface_plot":bonus}
+        #record={"Living_area": salary, "Bedrooms":tax, "Surface_plot":bonus}
         jprint(salary, tax, bonus )
-        price = clf.predict([[key for key in record.values()]])
+        price = 288
+        #price = clf.predict([[key for key in record.values()]])
         return redirect(url_for('home', price = 'Predicted price ' + str(int(price))+ '€'))
     else:
         salary = request.args.get('salary')
@@ -78,18 +80,22 @@ def chart3():
         lst_data1 = pd.DataFrame(lst_data1)
         df = lst_data1.copy()
         headers=df.iloc[0]
-        df  = pd.DataFrame(df.values[1:], columns=headers)
+        #df  = pd.DataFrame(df.values[1:], columns=headers)
+        df  = pd.DataFrame(df.to_numpy()[1:], columns=headers)
         df['Websites'] = df["Websites"].str.replace(',','')
         df['Websites'] = pd.to_numeric(df['Websites'])
         fig = px.bar(df[:-1], x="Year (June)", y="Websites", barmode = 'group')
-        graphJSON = json.dumps(fig, cls=plotly.utils.PlotlyJSONEncoder)
+        graph_json = json.dumps(fig, cls=plotly.utils.PlotlyJSONEncoder)
         header="Distribution online Webpages"
         description = """
         The idea that the number of webpages is decreasing can probably not be
         explained by this chart.
         """
-        return render_template('chart3.html', graphJSON=graphJSON, header=header,description=description)
+        return render_template('chart3.html', graphJSON=graph_json, header=header,description=description)
 
+@app.route('/images_predict')
+def images():
+    return render_template('images_predict.html')
 
 if __name__ == '__main__':
     app.run()
